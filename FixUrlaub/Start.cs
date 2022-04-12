87d2229fef8709d371d66b86d26cadbba77637b2
SlamWeasel <63350349+SlamWeasel@users.noreply.github.com>
2022-04-12 17:36:21 +0200

    Updated Language File

--- FixUrlaub/Start.cs
+++ FixUrlaub/Start.cs
@@ -32,10 +32,10 @@ namespace FixUrlaub
 
             if (!File.Exists(Environment.CurrentDirectory + "\\lang-DE.txt"))
                 File.WriteAllText(Environment.CurrentDirectory + "\\lang-DE.txt",
-                    "Deutsch;Urlaubsantrag<br/>für Mitarbeiter;Tag;Tage;Name;geb. am;Kontroll-Nr.;Abteilung;Mir zustehender Urlaub für das Jahr; bereits genommen, es verbleibt ein Rest von;Heute beantrage ich den nachstehend aufgeführten Urlaub;vom;bis;Jahresurlaub;Sonderurlaub;Unbezahlter Urlaub;Begründung;Arbeitnehmer bzw. Antragsteller;Datum;Unterschrift;geprüft: Vorgesetzter;Lohn-/Personalbüro;Einreichen;Fenster schließen");
+                    "Deutsch;Urlaubsantrag<br/>für Mitarbeiter;Tag;Tage;Name;geb. am;Kontroll-Nr.;Abteilung;Mir zustehender Urlaub für das Jahr; bereits genommen, es verbleibt ein Rest von;Heute beantrage ich den nachstehend aufgeführten Urlaub;vom;bis;Jahresurlaub;Sonderurlaub;Unbezahlter Urlaub;Begründung;Arbeitnehmer bzw. Antragsteller;Datum;Unterschrift;geprüft: Vorgesetzter;Lohn-/Personalbüro;Einreichen;Fenster schließen;Einstellungen;Ändern sie die Parameter des Programms, und sogar die Farbgebung;Kalender;Genehmigen");
             if (!File.Exists(Environment.CurrentDirectory + "\\lang-EN.txt"))
                 File.WriteAllText(Environment.CurrentDirectory + "\\lang-EN.txt",
-                    "English;Vacation Request<br/>for employees;Day;Days;Name;born on;User-ID;Department;My vacation for the year;already taken, there remain;Today, I request the following vacation time;from;to;Annual leave;Special leave;Unpaid vacation;Reason;Employee or Applicant;Date;Signature;checked: Superior;Human Resources;Submit;Exit Application");
+                    "English;Vacation Request<br/>for employees;Day;Days;Name;born on;User-ID;Department;My vacation for the year;already taken, there remain;Today, I request the following vacation time;from;to;Annual leave;Special leave;Unpaid vacation;Reason;Employee or Applicant;Date;Signature;checked: Superior;Human Resources;Submit;Exit Application;Settings;Change parameters of the program and even the color theme;Calendar;Approve");
 
             VacMainForm v = new VacMainForm();
             Application.Run(v);
