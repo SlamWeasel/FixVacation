6a644857b5a6760057b9f998eea6c15430e43277
SlamWeasel <63350349+SlamWeasel@users.noreply.github.com>
2022-04-14 17:31:57 +0200

    Update Start.cs

--- FixUrlaub/Start.cs
+++ FixUrlaub/Start.cs
@@ -1,4 +1,4 @@
-﻿using FixUrlaub.Masks;
+using FixUrlaub.Masks;
 using FixUrlaub.Util;
 using System;
 using System.Collections.Generic;
@@ -13,14 +13,17 @@ namespace FixUrlaub
 {
     internal class Start
     {
+        [STAThread]
         public static void Main(string[] args)
         {
             Console.WriteLine("Bruh");
+            ADUser me = new ADUser();
 
             try
             {
-                ADUser me = new ADUser("SloDuca");
-                Console.WriteLine(me.FullName + " -> " + me.ID + " workin in " + me.Department + " is Leader(" + me.IsLeader.ToString() + ") and works for " + me.Leader.FullName);
+                //me = new ADUser("MHahn");
+                //me = new ADUser(Environment.UserName);
+                Console.WriteLine(me.FullName + " -> " + me.ID + " workin in " + me.Department + " is Leader(" + me.IsLeader.ToString() + ") and works for " + (me.Leader == null ? "" : me.Leader.FullName));
 
                 ColorTheme colorTheme = new ColorTheme(ColorTheme.DefaultBackground, ColorTheme.DefaultForeground, ColorTheme.DefaultHighlight, new KeyValuePair<string, Color>("Alert", Color.Red), new KeyValuePair<string, Color>("Good", Color.FromArgb(0, 255, 0)));
                 Console.WriteLine(new ColorTheme(colorTheme.ToString()).ToString());
@@ -32,12 +35,12 @@ namespace FixUrlaub
 
             if (!File.Exists(Environment.CurrentDirectory + "\\lang-DE.txt"))
                 File.WriteAllText(Environment.CurrentDirectory + "\\lang-DE.txt",
-                    "Deutsch;Urlaubsantrag<br/>für Mitarbeiter;Tag;Tage;Name;geb. am;Kontroll-Nr.;Abteilung;Mir zustehender Urlaub für das Jahr; bereits genommen, es verbleibt ein Rest von;Heute beantrage ich den nachstehend aufgeführten Urlaub;vom;bis;Jahresurlaub;Sonderurlaub;Unbezahlter Urlaub;Begründung;Arbeitnehmer bzw. Antragsteller;Datum;Unterschrift;geprüft: Vorgesetzter;Lohn-/Personalbüro;Einreichen;Fenster schließen;Einstellungen;Ändern sie die Parameter des Programms, und sogar die Farbgebung;Kalender;Genehmigen");
+                    "Deutsch;Urlaubsantrag<br/>für Mitarbeiter;Tag;Tage;Name;geb. am;Kontroll-Nr.;Abteilung;Mir zustehender Urlaub für das Jahr; bereits genommen, es verbleibt ein Rest von;Heute beantrage ich den nachstehend aufgeführten Urlaub;vom;bis;Jahresurlaub;Sonderurlaub;Unbezahlter Urlaub;Begründung;Arbeitnehmer bzw. Antragsteller;Datum;Unterschrift;geprüft: Vorgesetzter;Lohn-/Personalbüro;Einreichen;Fenster schließen;Einstellungen;Ändern sie die Parameter des Programms, und sogar die Farbgebung;Kalender;Genehmigen;Sprache;Benutzerdef.;Farbschema;Hintergrundfarbe;Vordergrundfarbe;Detailfarbe");
             if (!File.Exists(Environment.CurrentDirectory + "\\lang-EN.txt"))
                 File.WriteAllText(Environment.CurrentDirectory + "\\lang-EN.txt",
-                    "English;Vacation Request<br/>for employees;Day;Days;Name;born on;User-ID;Department;My vacation for the year;already taken, there remain;Today, I request the following vacation time;from;to;Annual leave;Special leave;Unpaid vacation;Reason;Employee or Applicant;Date;Signature;checked: Superior;Human Resources;Submit;Exit Application;Settings;Change parameters of the program and even the color theme;Calendar;Approve");
+                    "English;Vacation Request<br/>for employees;Day;Days;Name;born on;User-ID;Department;My vacation for the year;already taken, there remain;Today, I request the following vacation time;from;to;Annual leave;Special leave;Unpaid vacation;Reason;Employee or Applicant;Date;Signature;checked: Superior;Human Resources;Submit;Exit Application;Settings;Change parameters of the program and even the color theme;Calendar;Approve;Language;Custom;Color Theme;Background Color;Foreground Color;Highlight Color");
 
-            VacMainForm v = new VacMainForm();
+            VacMainForm v = new VacMainForm(me);
             Application.Run(v);
 
         }
